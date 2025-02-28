5f0116207ed5d26c95373e9cafeda9709f0bd118
taiga1071101 <taigatukimi@gmail.com>
2025-03-01 00:24:18 +0900

    station6クリア

--- app/main.py
+++ app/main.py
@@ -9,6 +9,12 @@ from app.const import TodoItemStatusCode
 from .models.item_model import ItemModel
 from .models.list_model import ListModel
 
+from fastapi import Depends
+from .dependencies import get_db    # 依存関係を注入
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+
 DEBUG = os.environ.get("DEBUG", "") == "true"
 
 app = FastAPI(
@@ -85,3 +91,15 @@ def get_echo(message: str, name: str):
 @app.get("/health", tags=["System"])
 def get_health():
     return {"status": "ok"}
+
+@app.get("/lists/{todo_list_id}", tags=["Todoリスト"])
+def get_todo_list(todo_list_id: int, db: Session = Depends(get_db)):
+    # db_item = Session.query(ListModel).filter(ListModel.id == todo_list_id).first()
+    stmt = select(ListModel).where(ListModel.id == todo_list_id)
+    result = db.execute(stmt)
+    db_item = result.scalar_one_or_none()
+
+    if db_item is None:
+        return {"error": "Todo list not found"}
+    
+    return db_item
